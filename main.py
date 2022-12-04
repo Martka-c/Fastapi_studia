b23fcc1ac6ca8ad5ccb710b68a7b2e6b96853036
Martka <stiebler@poczta.fm>
2022-12-04 19:59:54 +0100

    Working authorized endpoint with datetime

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -11,10 +13,9 @@ async def token(login_form: OAuth2PasswordRequestForm = Depends()):
     return {"access_token": login_form.username + "_token"}
 
 
-@app.get("/prime/{number}")
+@app.get("/current_datatime")
 async def index(token_value: str = Depends(oauth2_scheme)):
-    #MS TODO: here call processing method
-    return {"ready_token": token_value}
+    return {"current_time": datetime.now()}
 
 
 @app.get("/prime/{number}")
